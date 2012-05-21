ba488587d91ebb7845b69419e0321200239a798b
dsanchez <dsanchez@106-16.info2>
2012-05-21 12:04:18 +0200

    prueba

---
+++ versiones/src/prueba_versiones.java
@@ -0,0 +1,8 @@
+
+public class prueba_versiones {
+
+	public static void main(String[] args) {
+		
+		System.out.println("prueba");
+	}
+}
---
+++ versiones/src/prueba_versiones2.java
@@ -0,0 +1,8 @@
+
+public class prueba_versiones2 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
